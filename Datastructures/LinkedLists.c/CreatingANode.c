4bc13e215bf90f505f1e93ce6a34b9f23abb1277
Aditya Mali <adityamali33@gmail.com>
2020-09-01 19:00:50 +0530

    insert at end linklist using c

--- Datastructures/LinkedLists.c/CreatingANode.c
+++ Datastructures/LinkedLists.c/CreatingANode.c
@@ -1,14 +1,45 @@
-#include <stdio.h>
-
+ #include <stdio.h>
+#include <stdlib.h>
 struct node{
 
-    int data = NULL;
-    int *link;
-}n;
+    int data ;
+    struct node *link;
+};
 
-int main(){
-    struct n.data = 20;
+struct node *start = NULL;  
 
-    printf("%d",n.data);
+struct node *createNode(){
+
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+
+    return(n);
+}
+
+void insertNode(){
+
+    struct node *temp = createNode();
+
+    printf("\nEnter a number: ");
+    scanf("%d",&temp->data);
+    temp->link = NULL;
+
+    if(start == NULL){
+        start = temp;
+    }else{
+        struct node *lastnode = start;
+        while(lastnode->link != NULL){
+            lastnode = lastnode->link;
+        }
+        lastnode->link = temp;
+    }
+}
+
+
+
+void main(){
+
+insertNode();
+
 
 }
